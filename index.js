83b846596a9c282b42d4b27958f825247a3b441d
Odima-dev <vincentodima22@gmail.com>
2025-02-23 23:15:23 +0300

    setting up firebase realtime url

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-database.js"
 
 const firebaseConfig = {
     databaseURL: "https://leads-tracker-app-7acd6-default-rtdb.firebaseio.com/"
 }
 
 const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+
+console.log(database)
 
 console.log(app)
 
