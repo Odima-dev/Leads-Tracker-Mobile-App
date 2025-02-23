1a595579ea5c2a619471864aef5008ea1d617044
Odima-dev <vincentodima22@gmail.com>
2025-02-23 23:43:20 +0300

    deleting unneeded code

--- index.js
+++ index.js
@@ -10,20 +10,10 @@ const database = getDatabase(app)
 
 console.log(app)
 
-let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el")
-const tabBtn = document.getElementById("tab-btn");
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage;
-    render(myLeads);
-}
-
-
 
 function render(leads) {
     let listItems = ""
@@ -40,14 +30,10 @@ function render(leads) {
 }
 
 deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
     render(myLeads)
 })
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
+    console.log(inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
 })
