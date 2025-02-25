c2298d181a80bd762d7a25dd67fda92e74ae8965
Odima-dev <vincentodima22@gmail.com>
2025-02-25 15:17:41 +0300

    replacing .env with config file to secure environment variable

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ import { getDatabase,
     push
  } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-database.js";
 
-
-const firebaseConfig = {
-    databaseURL: process.env.DATABASE_URL
-}
-
 const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
 const referenceInDB = ref(database, "leads")
